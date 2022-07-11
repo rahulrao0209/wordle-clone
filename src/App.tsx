6cfa0ca8bcda0a3cdae1b0abe201d8ee2d44982e
unknown <rahulrao0209@gmail.com>
2022-07-11 21:11:58 +0530

    mocking a server using json-server | Get response from the server which contains all our hardcoded json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const App = () => {
+
+  const [solution, setSolution] = useState(null);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+
+      try {
+        const response = await fetch("http://localhost:3000/solutions");
+        const data = await response.json();
+        const randomSolution = data[Math.floor(Math.random() * data.length)];
+        setSolution(randomSolution.word);
+      } catch(err) {
+        console.log("Error fetching data: ", err);
+      }
+    }
+
+    fetchData();
+  }, [setSolution]);
+
   return (
     <div className="app">
       <h1>Wordle</h1>
